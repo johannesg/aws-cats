f7fefb27fe7ded32100e928f6e8126d3a72d64b3
Johannes Gustafsson <johannes@jogus.io>
2021-01-13 22:03:39 +0100

    app

--- cdk/lib/static-site.ts
+++ cdk/lib/static-site.ts
@@ -83,14 +83,14 @@ export class StaticSite extends Construct {
         new cdk.CfnOutput(this, 'AppCodeBucketName', { value: source.bucketName });
         new cdk.CfnOutput(this, 'AppCodeObjectKey', { value: source.objectKey });
 
-        // const sourceBucket = s3.Bucket.fromBucketName(this, 'AppCodeBucket', source.bucketName);
+        const sourceBucket = s3.Bucket.fromBucketName(this, 'AppCodeBucket', source.bucketName);
 
-        // // Deploy site contents to S3 bucket
-        // new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
-        //     sources: [ s3deploy.Source.bucket(sourceBucket, source.objectKey) ],
-        //     destinationBucket: siteBucket,
-        //     distribution,
-        //     distributionPaths: ['/*'],
-        //   });
+        // Deploy site contents to S3 bucket
+        new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
+            sources: [ s3deploy.Source.bucket(sourceBucket, source.objectKey) ],
+            destinationBucket: siteBucket,
+            distribution,
+            distributionPaths: ['/*'],
+          });
     }
 }
