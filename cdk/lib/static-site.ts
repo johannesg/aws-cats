926967897ce1d83b5b1abda99a71c283b12c338e
Johannes Gustafsson <johannes@jogus.io>
2021-01-14 10:45:20 +0100

    stuff

--- cdk/lib/static-site.ts
+++ cdk/lib/static-site.ts
@@ -37,15 +37,15 @@ export class StaticSite extends Construct {
 
         // Content bucket
         const siteBucket = new s3.Bucket(this, 'SiteBucket', {
-            bucketName: domainName,
-            websiteIndexDocument: 'index.html',
-            websiteErrorDocument: 'error.html',
-            publicReadAccess: true,
+            // bucketName: domainName,
+            // websiteIndexDocument: 'index.html',
+            // websiteErrorDocument: 'error.html',
+            // publicReadAccess: true,
 
-            // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
-            // the new bucket, and it will remain in your account until manually deleted. By setting the policy to
-            // DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
-            removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+            // // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+            // // the new bucket, and it will remain in your account until manually deleted. By setting the policy to
+            // // DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
+            // removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
         });
 
         new cdk.CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
@@ -60,6 +60,9 @@ export class StaticSite extends Construct {
             },
             originConfigs: [
                 {
+                    s3OriginSource: {
+                        s3BucketSource: siteBucket
+                    },
                     customOriginSource: {
                         domainName: siteBucket.bucketWebsiteDomainName,
                         originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
@@ -73,12 +76,12 @@ export class StaticSite extends Construct {
         });
         new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
 
-        // Route53 alias record for the CloudFront distribution
-        new route53.ARecord(this, 'SiteAliasRecord', {
-            recordName: domainName,
-            target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
-            zone
-        });
+        // // Route53 alias record for the CloudFront distribution
+        // new route53.ARecord(this, 'SiteAliasRecord', {
+        //     recordName: domainName,
+        //     target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+        //     zone
+        // });
 
         new cdk.CfnOutput(this, 'AppCodeBucketName', { value: source.bucketName });
         new cdk.CfnOutput(this, 'AppCodeObjectKey', { value: source.objectKey });
@@ -87,10 +90,10 @@ export class StaticSite extends Construct {
 
         // Deploy site contents to S3 bucket
         new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
-            sources: [ s3deploy.Source.bucket(sourceBucket, source.objectKey) ],
+            sources: [s3deploy.Source.bucket(sourceBucket, source.objectKey)],
             destinationBucket: siteBucket,
             distribution,
             distributionPaths: ['/*'],
-          });
+        });
     }
 }
