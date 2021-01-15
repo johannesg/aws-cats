357e20fb7fac3807ee3bee4d827db5762d9489d7
Johannes Gustafsson <johannes@jogus.io>
2021-01-15 16:38:02 +0100

    app

--- app/client/plugins/amplify/aws-config.js
+++ app/client/plugins/amplify/aws-config.js
@@ -1,5 +1,5 @@
 export default {
     region: 'eu-north-1',
-    userPoolId: 'eu-north-1_q4lnBAOyq',
-    userPoolWebClientId: '62uvudrk7regjgdkof44ia49vq'
+    userPoolId: 'eu-north-1_V2HbWL624',
+    userPoolWebClientId: 'im6h34cirsr5mfgudg76395vo'
 }
