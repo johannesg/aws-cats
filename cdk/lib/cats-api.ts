03d68238af312844b3232e9a412df6d4f0673113
Johannes Gustafsson <johannes@jogus.io>
2021-01-16 19:59:16 +0100

    cdk

--- cdk/lib/cats-api.ts
+++ cdk/lib/cats-api.ts
@@ -29,7 +29,7 @@ export class CatsApi extends cdk.Construct {
         })
 
         const authorizerId = new CfnAuthorizer(this, "Authorizer", {
-            name: "cats-cognito-authorizer",
+            name: "cats-api-cognito-authorizer",
             identitySource: "method.request.header.Authorization",
             providerArns: [auth.userPool.userPoolArn],
             restApiId: api.restApiId,
